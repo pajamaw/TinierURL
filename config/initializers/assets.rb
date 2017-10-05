1d8db254bc5beecbefbeb84efa0a6034bec7cb5c
PJ Wickwire <pjpeterww@gmail.com>
2017-10-05 09:52:15 -0400

    changed manifest to include fontawesome

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,9 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+%w(eot svg ttf woff woff2).each do |ext|
+  Rails.application.config.assets.precompile << "fontawesome-webfont.#{ext}"
+end
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
